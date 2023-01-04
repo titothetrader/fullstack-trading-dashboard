a72e65345ed8b030ce04b94e2bf91da38709ed2c
Tito Sanchez <titothetrader@gmail.com>
2023-01-04 18:35:26 -0500

    added components index

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+import { Navbar } from './components';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="text-center">
-      <header className="App-header">
-        <h1 className="text-3xl underline text-white">Header</h1>
+      <header className="bg-blue-400">
+        <Navbar />
       </header>
       <div>
+        <h1 className="text-3xl underline text-white">Header</h1>
         <h1 className="text-2xl text-blue-400">Body</h1>
       </div>
     </div>
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div className="h-24 pt-8">
+            <h1 className="text-3xl text-white underline">Navbar</h1>
+        </div>
+    )
+}
+
+export default Navbar
