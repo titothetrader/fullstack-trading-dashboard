287888ee50d8d42582f53a6aa6791fd0e4194833
Tito Sanchez <titothetrader@gmail.com>
2023-01-05 13:08:25 -0500

    improved backend html view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import { useEffect, useState } from 'react'
 import { Navbar } from './components';
 import { getStocks } from './services/stocksAPI'
 
-import './App.css';
-
 function App() {
   const [stocks, setStocks] = useState()
 
@@ -23,7 +21,7 @@ function App() {
         <Navbar />
       </header>
       <div>
-      <h1 className="text-3xl underlie text-white">Stocks</h1>
+        <h1 className="text-3xl underlie text-white">Stocks</h1>
         {stocks?.map((stock) => (
           <li key={stock.id} className="text-2xl text-blue-400">{stock.id}. {stock.symbol}</li>
         ))}
