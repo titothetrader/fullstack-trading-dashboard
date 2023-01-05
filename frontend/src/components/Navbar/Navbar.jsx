96b5eb854c1069b7ad00953b2ed7787660b1e185
Tito Sanchez <titothetrader@gmail.com>
2023-01-05 15:28:52 -0500

    redux

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,10 +2,18 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div className="nav-title">
-            <h1 className="text-3xl black underline">
-                <Link to="/">Navbar</Link>
-            </h1>
+        <div className="nav-bar">
+                <div className='flex'>
+                    <img src="#"/>
+                </div>
+            <div className=''>
+                <ul className='flex justify-end mb-2'>
+                    <li className="mr-6"><Link to="/">Home</Link></li>
+                    <li className="mr-6"><Link to="/crypto">Crypto</Link></li>
+                    <li className="mr-6"><Link to="/fx">Forex</Link></li>
+                    <li className="mr-6"><Link to="/stocks">Stocks</Link></li>
+                </ul>
+            </div>
         </div>
     )
 }
